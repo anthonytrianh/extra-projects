626a0f0518cd4da78b3633ab04b209ee2fb130d0
Anh "Anthony" Nguyen <anthony.trianh@gmail.com>
2024-12-21 02:20:59 -0500

    Water Drips on rope

---
+++ Assets/Purple Rain/Scripts/RopePoint.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using GogoGaga.OptimizedRopesAndCables;
+using UnityEngine;
+
+[ExecuteAlways]
+public class RopePoint : MonoBehaviour
+{
+    [SerializeField] Rope rope;
+
+    [SerializeField][Range(0f, 1f)] float pointLocationOnRope = 0.1f;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Application.isPlaying)
+        {
+            UpdatePositionOnRope();
+        }
+    }
+
+    void OnValidate()
+    {
+        UpdatePositionOnRope();
+    }
+
+    void UpdatePositionOnRope()
+    {
+        if (rope == null)
+        {
+            return;
+        }
+
+        Vector3 location = rope.GetPointAt(pointLocationOnRope);
+        transform.position = location;
+    }
+}
